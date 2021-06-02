d8e876eb17fbff5938036d3bb709303b6e8a1620
Weiyi Zhu <wzhu1@swarthmore.edu>
2021-06-02 07:31:52 -0500

    create a user model

---
+++ backend/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = Exercise 
