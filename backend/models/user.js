f59ea9eb4cac4db530311de31e137ac3824b6bba
Weiyi Zhu <wzhu1@swarthmore.edu>
2021-06-06 16:44:06 -0500

    get rid of username

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,12 +3,6 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
     email: {
       type: String,
       required: true,
