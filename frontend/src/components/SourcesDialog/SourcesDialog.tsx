02c854bb7d80d3a900572d209a2b23caea130d4f
Weiyi Zhu <zhuwy99@gmail.com>
2021-06-28 08:50:56 -0500

    SourcesDialog component

---
+++ frontend/src/components/SourcesDialog/SourcesDialog.tsx
@@ -0,0 +1,47 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@material-ui/core";
+import { ParsableDate } from "@material-ui/pickers/constants/prop-types";
+import { Sources, StateProps } from "../Search/Search";
+import React from "react";
+
+interface Props {
+  values: StateProps;
+  setValues: React.Dispatch<React.SetStateAction<StateProps>>;
+}
+
+const SourcesDialog: React.FC<Props> = ({ values, setValues }: Props) => {
+  return (
+    <Dialog
+      open={values.openDialogue}
+      onClose={() => {
+        setValues({ ...values, openDialogue: false });
+      }}
+    >
+      <DialogTitle>Sources</DialogTitle>
+      <DialogContent></DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => setValues({ ...values, openDialogue: false })}
+          color="primary"
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={() => {
+            setValues({ ...values, openDialogue: false });
+          }}
+          color="primary"
+        >
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default SourcesDialog;
