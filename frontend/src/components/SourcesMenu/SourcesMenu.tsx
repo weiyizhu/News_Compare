b08eba27f86f8ea5426dac3f9bc12abf317e4735
Weiyi <zhuwy99@gmail.com>
2021-07-04 23:38:39 -0500

    autocomplete with sources selection

--- frontend/src/components/SourcesMenu/SourcesMenu.tsx
+++ frontend/src/components/SourcesMenu/SourcesMenu.tsx
@@ -1,9 +1,23 @@
-import { Checkbox, FormControlLabel, Menu, MenuItem } from "@material-ui/core";
+import {
+  Button,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControlLabel,
+  Menu,
+  MenuItem,
+  Slider,
+  TextField,
+} from "@material-ui/core";
 import { ParsableDate } from "@material-ui/pickers/constants/prop-types";
 import { StateProps } from "../Search/Search";
 import React from "react";
-import { EventNoteRounded } from "@material-ui/icons";
+import { CheckBoxOutlineBlank, EventNoteRounded } from "@material-ui/icons";
 import { allSources } from "../../static/allSources";
+import { Autocomplete } from "@material-ui/lab";
+import { checkUtils } from "@material-ui/pickers/_shared/hooks/useUtils";
 
 interface Props {
   values: StateProps;
@@ -45,30 +59,108 @@ const SourcesMenu: React.FC<Props> = ({ values, setValues }: Props) => {
       });
   };
 
+  let defaultValues = [];
+  for (let src of values.sources) {
+    defaultValues.push(allSources.find((allSrc) => allSrc.id === src));
+  }
+
   return (
-    <Menu
+    // https://material-ui.com/components/autocomplete/#checkboxes
+    <Dialog
       open={values.openMenu}
-      onClose={handleClose}
-      style={{ maxHeight: 300, minWidth: 1000 }}
+      onClose={() => {
+        setValues({ ...values, openMenu: false });
+      }}
     >
-      {/* <Slider step={null} marks={marks} style={{ width: "80%", left: "10%" }} /> */}
-      {allSources?.map((entry) => {
-        return (
-          <MenuItem key={entry.id}>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  name={entry.id}
-                  checked={values.sources.includes(entry.id)}
-                  onChange={handleCheckBoxClick}
-                />
+      <DialogTitle>SOURCES</DialogTitle>
+      <DialogContent>
+        <Slider
+          step={null}
+          marks={marks}
+          style={{ width: "80%", left: "10%" }}
+        />
+        <Autocomplete
+          multiple
+          id="checkboxes-tags-demo"
+          options={allSources}
+          disableCloseOnSelect
+          getOptionLabel={(options) => (options ? options.name : "None")}
+          renderOption={(option, { inputValue, selected }) => {
+            return (
+              option && (
+                <>
+                  <Checkbox checked={selected} style={{ marginRight: 8 }} />{" "}
+                  {option.name}
+                </>
+              )
+            );
+          }}
+          style={{ minWidth:"275px" }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              label="Sources"
+              error={values.sources.length > 3}
+              helperText={
+                values.sources.length > 3 &&
+                "Sources cannot be more than three."
               }
-              label={entry.name}
             />
-          </MenuItem>
-        );
-      })}
-    </Menu>
+          )}
+          defaultValue={defaultValues}
+          limitTags={3}
+          onChange={(event, selectedSourcesObj) => {
+            console.log(event, selectedSourcesObj);
+            let selectedSourcesArr: string[] = [];
+            selectedSourcesObj.map((src) => {
+              src && selectedSourcesArr.push(src.id);
+            });
+            setValues({ ...values, sources: selectedSourcesArr });
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => setValues({ ...values, openMenu: false })}
+          color="primary"
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={() => {
+            setValues({ ...values, openMenu: false });
+          }}
+          color="primary"
+        >
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+
+    // <Menu
+    //   open={values.openMenu}
+    //   onClose={handleClose}
+    //   style={{ maxHeight: 300, minWidth: 1000 }}
+    // >
+    //   {/* <Slider step={null} marks={marks} style={{ width: "80%", left: "10%" }} /> */}
+    //   {allSources?.map((entry) => {
+    //     return (
+    //       <MenuItem key={entry.id}>
+    //         <FormControlLabel
+    //           control={
+    //             <Checkbox
+    //               name={entry.id}
+    //               checked={values.sources.includes(entry.id)}
+    //               onChange={handleCheckBoxClick}
+    //             />
+    //           }
+    //           label={entry.name}
+    //         />
+    //       </MenuItem>
+    //     );
+    //   })}
+    // </Menu>
     // <Dialog
     //   open={values.openMenu}
     //   onClose={() => {
