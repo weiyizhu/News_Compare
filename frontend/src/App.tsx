50721437b94ecc59707812c216c2ebcac940cc14
Weiyi <zhuwy99@gmail.com>
2021-08-18 18:20:08 -0700

    Add react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
+import SignUp from "./components/SignUp";
 import News from "./screens/News";
 
 function App() {
   return (
-    <div>
-      <News />
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={News} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
