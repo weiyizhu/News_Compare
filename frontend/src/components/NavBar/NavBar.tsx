43e2b6b8861dd9e163b1a3a4246206d7d4170299
Weiyi <zhuwy99@gmail.com>
2021-08-24 20:25:20 -0700

    NavBar drop down options for logged in user

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   AppBar,
+  Avatar,
   Button,
+  ClickAwayListener,
+  Divider,
+  Grow,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   makeStyles,
+  Menu,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popover,
+  Popper,
   Toolbar,
   Typography,
 } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../state/reducers";
+import { actionCreators } from "../../state";
+import {
+  AccountCircle,
+  Apps,
+  ChromeReaderMode,
+  Dashboard,
+  Description,
+  Drafts,
+  ExitToApp,
+  Inbox,
+  Search,
+  Send,
+} from "@material-ui/icons";
+import { Link, NavLink } from "react-router-dom";
+import { id } from "date-fns/locale";
 
 const useStyles = makeStyles({
   title: {
@@ -15,13 +47,85 @@ const useStyles = makeStyles({
 
 const NavBar: React.FC = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector<RootState, boolean>(
+    (state) => state.user.loggedIn
+  );
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  useEffect(() => {
+    dispatch(actionCreators.checkLoggedInStatus());
+  }, []);
   return (
     <AppBar position="sticky" style={{ marginBottom: "1em" }}>
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
           News Compare
         </Typography>
-        <Button color="inherit">Log In</Button>
+        {isLoggedIn ? (
+          <>
+            <IconButton onClick={handleMenu} color="inherit">
+              <Avatar>H</Avatar>
+            </IconButton>
+            <Popover
+              open={Boolean(anchorEl)}
+              anchorEl={anchorEl}
+              onClose={() => {
+                setAnchorEl(null);
+              }}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+            >
+              <List>
+                <ListItem button>
+                  <ListItemIcon>
+                    <AccountCircle />
+                  </ListItemIcon>
+                  <ListItemText primary="Account" />
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <Search />
+                  </ListItemIcon>
+                  <ListItemText primary="Saved Searches" />
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <Description />
+                  </ListItemIcon>
+                  <ListItemText primary="Saved Posts" />
+                </ListItem>
+                <Divider />
+                <ListItem
+                  button
+                  onClick={() => dispatch(actionCreators.logout())}
+                >
+                  <ListItemIcon>
+                    <ExitToApp />
+                  </ListItemIcon>
+                  <ListItemText primary="Sign out" />
+                </ListItem>
+              </List>
+            </Popover>
+          </>
+        ) : (
+          <Button color="inherit">
+            <NavLink
+              to="/login"
+              activeStyle={{ color: "white" }}
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              {"Log In/Sign Up"}
+            </NavLink>
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
