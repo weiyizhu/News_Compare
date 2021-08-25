a30e23d60cf207b8456995a74925502ece6c482a
Weiyi <zhuwy99@gmail.com>
2021-08-24 20:24:37 -0700

    Account component styles

---
+++ frontend/src/components/Account/styles.ts
@@ -0,0 +1,11 @@
+import { createStyles } from "@material-ui/core";
+import theme from "../../themes/theme";
+
+const styles = createStyles({
+  x: {
+    justifyContent: "flex-end",
+    padding: "1em",
+  },
+});
+
+export default styles;
