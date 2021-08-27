c0c95a7a699d0f3dc6f2d73b22f963749c9a9168
Weiyi <zhuwy99@gmail.com>
2021-08-26 18:04:10 -0700

    News screen

--- frontend/src/screens/News.tsx
+++ frontend/src/screens/News.tsx
@@ -1,17 +1,11 @@
-import React from "react";
 import DisplayNews from "../components/DisplayNews";
-import Login from "../components/Login";
-import NavBar from "../components/NavBar";
 import Search from "../components/Search";
-import SignUp from "../components/SignUp";
 
 const News = () => {
   return (
     <>
-      {/* <NavBar />
-      <Search /> */}
-      {/* <Login /> */}
-      <SignUp />
+      <Search />
+      <DisplayNews />
     </>
   );
 };
